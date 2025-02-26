efaf4cff9b50501acc4ad41ef3f724436a4d049e
Alejandro <alebernadev@gmail.com>
2025-02-26 17:49:18 +0100

    Conecto el codigo a la BBDD

--- app/src/main/java/com/example/listadelacompra/GestorBaseDatos.java
+++ app/src/main/java/com/example/listadelacompra/GestorBaseDatos.java
@@ -1,10 +1,13 @@
 package com.example.listadelacompra;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
 
 public class GestorBaseDatos extends SQLiteOpenHelper {
 
@@ -17,11 +20,37 @@ public class GestorBaseDatos extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
 
         db.execSQL("CREATE TABLE productos (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, precio FLOAT, cantidad INTEGER)");
+        db.execSQL("INSERT INTO productos VALUES(null,'producto ej 1',30,5)");
+        db.execSQL("INSERT INTO productos VALUES(null,'producto ej 2',20,10)");
+        db.execSQL("INSERT INTO productos VALUES(null,'producto ej 3',10,6)");
+        db.execSQL("INSERT INTO productos VALUES(null,'producto ej 4',15,8)");
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
 
+    }
+
+    public ArrayList<String> obtenerProductos()
+    {
+        ArrayList<String> productos= new ArrayList<String>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cur = db.rawQuery("SELECT * FROM productos",null);
+        if(cur!=null){
+
+            cur.moveToFirst(); //Voy a la primera fila y entro en el bucle
+            while(!cur.isAfterLast())
+            {
+                //Codigo para obtener la informacion
+                productos.add(cur.getString(0) + "._" + cur.getString(1) + " (" + cur.getString(2) + " €) " + "(" + cur.getString(3) + " unidades)");
+
+
+                cur.moveToNext();
+            }
+            cur.close();
+        }
+        db.close();
+        return productos;
     }
 }
--- app/src/main/java/com/example/listadelacompra/MainActivity.java
+++ app/src/main/java/com/example/listadelacompra/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.listadelacompra;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -15,6 +17,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.awt.font.TextAttribute;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
     protected ListView lista1;
 
     protected String contenidoCaja1="";
+    protected GestorBaseDatos gdb;
+
+    protected ArrayList<String> listaProductos = new ArrayList<String>();
+    protected ArrayAdapter<String> adaptador;
 
 
     @Override
@@ -42,6 +49,11 @@ public class MainActivity extends AppCompatActivity {
         caja1 = (EditText) findViewById(R.id.caja1_main);
         boton1 = (Button) findViewById(R.id.boton1_main);
         lista1 = (ListView) findViewById(R.id.lista1_main);
+
+        gdb = new GestorBaseDatos(this); //Aqui es donde creo la BBDD y las tablas y solo se va hacer una vez
+        listaProductos = gdb.obtenerProductos();
+        adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listaProductos);
+        lista1.setAdapter(adaptador);
 
         boton1.setOnClickListener(new View.OnClickListener() {
             @Override
